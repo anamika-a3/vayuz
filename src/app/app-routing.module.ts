146c9086115a96f8de4bfde76606eae3cbf31a51
anamika-a3 <67058392+anamika-a3@users.noreply.github.com>
2020-07-23 21:07:32 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from "./app.component";
+import { BlogListingComponent } from "./blog-listing/blog-listing.component";
+import { CreateBlogComponent } from "./create-blog/create-blog.component";
+import { EditBlogComponent } from "./edit-blog/edit-blog.component";
+import { ViewBlogComponent } from "./view-blog/view-blog.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: AppComponent },
+  { path: "blogList", component: BlogListingComponent },
+  { path: "createBlog", component: CreateBlogComponent },
+  { path: "editBlog", component: EditBlogComponent },
+  { path: "viewBlog", component: ViewBlogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [
+  AppComponent,
+  BlogListingComponent,
+  CreateBlogComponent,
+  EditBlogComponent,
+  ViewBlogComponent,
+];
+
